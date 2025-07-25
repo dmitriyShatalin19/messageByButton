77e853daec6d3b5d20f83856569fb879515bc514
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-25 11:45:44 +0300

    сделал показ сообщение и обновление

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('log')
+const Text = document.querySelector('.content_text');
+const buttonShow = document.querySelector('.btn_show');
+const buttonUpdate = document.querySelector('.btn_update');
+
+let NewText = 'Ты нажал на кнопку';
+
+buttonShow.addEventListener('click', function() {
+    Text.textContent = NewText;
+})
+
+buttonUpdate.addEventListener('click', function() {
+    Text.textContent = 'ПУСТО';
+});
