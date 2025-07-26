ff5e847d23cf974778301107aabde89826e20306
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-26 17:05:46 +0300

    сделал темный и светлый режим

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const Text = document.querySelector('.content_text');
 const buttonShow = document.querySelector('.btn_show');
 const buttonUpdate = document.querySelector('.btn_update');
+const bodyLinght = document.querySelector('.light-theme');
+const bodyDark = document.querySelector('body');
 
 let NewText = 'Ты нажал на кнопку';
 
@@ -11,3 +13,10 @@ buttonShow.addEventListener('click', function() {
 buttonUpdate.addEventListener('click', function() {
     Text.textContent = 'ПУСТО';
 });
+
+bodyLinght.addEventListener('click', function() {
+    bodyLinght.classList.toggle('light-color');
+})
+bodyDark.addEventListener('click', function() {
+    bodyDark.classList.toggle('dark-color');
+})
