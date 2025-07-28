80c4b85905e98f108517e010f9dde4cbb87470d0
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-28 15:53:27 +0300

    рефакторинг темы

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 const Text = document.querySelector('.content_text');
 const buttonShow = document.querySelector('.btn_show');
 const buttonUpdate = document.querySelector('.btn_update');
-const bodyLinght = document.querySelector('.light-theme');
-const bodyDark = document.querySelector('body');
 
 let NewText = 'Ты нажал на кнопку';
 
@@ -14,9 +12,16 @@ buttonUpdate.addEventListener('click', function() {
     Text.textContent = 'ПУСТО';
 });
 
-bodyLinght.addEventListener('click', function() {
-    bodyLinght.classList.toggle('light-color');
-})
-bodyDark.addEventListener('click', function() {
-    bodyDark.classList.toggle('dark-color');
-})
+
+const btnLight = document.querySelector('.btn-light');
+const btnDark = document.querySelector('.btn-dark');
+
+btnLight.addEventListener('click', () => {
+  document.body.classList.remove('dark-mode');
+  document.body.classList.add('light-mode');
+});
+
+btnDark.addEventListener('click', () => {
+  document.body.classList.remove('light-mode');
+  document.body.classList.add('dark-mode');
+});
